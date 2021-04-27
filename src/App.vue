b0aedbaff9338da3e2d814afa6b8efa040493904
Shalaw Fatah <shalaw.fatah@gmail.com>
2021-04-26 23:18:59 -0700

    restarted app

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
     <h1>{{title}}</h1>
+    <Simple />
 </template>
 
 <script>
+import Simple from './components/Simple'
 export default {
     name: 'App',
     data() {
         return {
             title: 'My Vue App'
         }
+    }
+    components: {
+        name: 'Simple'
     }
 }
 </script>
---
+++ src/components/Simple.vue
@@ -0,0 +1,15 @@
+<template>
+<h1 className="heading">This is a component</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.heading {
+    color: crimson;
+}
+</style>
