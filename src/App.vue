e959df8e3c09f7b649a2bc0416a88b9c5883a66b
Shalaw Fatah <shalaw.fatah@gmail.com>
2021-04-26 23:13:02 -0700

    restarted app

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,16 @@
 <template>
-<div id="app">
-{{user.name}}
-</div>
+    <h1>My First Vue App</h1>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  data() {
-  return {
-    followers: 0,
-    user: {
-    id: 1,
-    name: "Shalaw Fatah",
-    firstName: "Shalaw",
-    lastName: "Fatah",
-    email: "shalaw.fatah@gmail.com",
-    isAdmin: true
-    }
-     }
-  }
-  computed: {
-  fullName() {
-  return `${this.user.firstName} ${this.user.lastName} `
-  }
+    name: 'App',
+
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+<style scoped>
+
 </style>
+
